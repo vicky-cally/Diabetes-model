eab792007a2142d8637fb2c54d4690e4e0b72d1c
Calista Dikeh <89668437+vicky-cally@users.noreply.github.com>
2024-03-29 22:47:57 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -4,7 +4,7 @@ from predict_page1 import show_predict_page
 from explore_page1 import show_explore_page
 import pandas as pd
 
-page= st.selectbox("Explore or Predict",("Predict","Explore"))
+page= st.sidebar.selectbox("Explore or Predict",("Predict","Explore"))
 # Load dataset
 #dataset = pd.read_csv("diabetes2.csv")
 #df = dataset.copy()
