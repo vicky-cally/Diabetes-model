8deeb6d43d880e23993e6d72489c872cd8a6b1d9
Calista Dikeh <89668437+vicky-cally@users.noreply.github.com>
2024-03-29 20:54:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,14 @@
 import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page
-import pandas as pd
+#import pandas as pd
 
 page= st.selectbox("Explore or Predict",("Predict","Explore"))
 # Load dataset
-dataset = pd.read_csv("diabetes2.csv")
-df = dataset.copy()
+#dataset = pd.read_csv("diabetes2.csv")
+#df = dataset.copy()
 
 if page == "Predict":
     show_predict_page()
 else:
-    show_explore_page(df)
+    show_explore_page()
