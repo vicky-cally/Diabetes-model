b6e7665efe3b1dfc4bea3bb9815fa31e79bb3b71
Calista Dikeh <89668437+vicky-cally@users.noreply.github.com>
2024-03-30 12:12:46 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -5,9 +5,6 @@ from explore_page1 import show_explore_page
 import pandas as pd
 
 page= st.sidebar.selectbox("Explore or Predict",("Predict","Explore"))
-# Load dataset
-#dataset = pd.read_csv("diabetes2.csv")
-#df = dataset.copy()
 
 if page == "Predict":
     show_predict_page()
