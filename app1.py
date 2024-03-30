653032df22bbe18cde7df5d6fe39efe3c9f282dc
Calista Dikeh <89668437+vicky-cally@users.noreply.github.com>
2024-03-30 22:35:48 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,12 +1,12 @@
 # app.py
 import streamlit as st
 from predict_page1 import show_predict_page
-#from explore_page1 import show_explore_page
+from explore_page1 import show_explore_page
 import pandas as pd
 
-#page= st.sidebar.selectbox("Explore or Predict",("Predict","Explore"))
+page= st.sidebar.selectbox("Explore or Predict",("Predict","Explore"))
 
-#if page == "Predict":
-show_predict_page()
-#else:
-    #show_explore_page()
+if page == "Predict":
+    show_predict_page()
+else:
+    show_explore_page()
